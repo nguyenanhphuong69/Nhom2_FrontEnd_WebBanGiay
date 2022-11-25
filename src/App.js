c695135602bddc0fb4dea8fc012684e72020da7c
nguyenanhphuong69 <anhphuongnguyen016@gmail.com>
2022-11-25 16:28:47 +0700

    setup frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Hello/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
